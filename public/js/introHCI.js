0038722452d8a067f5e5e01a590996ff26be6dfa
Akshay Buddiga <abuddiga@stanford.edu>
2014-02-14 13:34:34 -0800

    ajax lab

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -27,6 +27,17 @@ function addProjectDetails(e) {
 	var idNumber = projectID.substr('project'.length);
 
 	console.log("User clicked on project " + idNumber);
+
+	$.get("/project/"+idNumber, showDetails);
+}
+
+function showDetails(result) {
+	var detailsID = '#project' + result['id'] + " .details";
+	var detailsDiv = $(detailsID);
+	detailsDiv.html('<a href="#" class="thumbnail">' + 
+		'<img src=' + result['image'] + ' class="detailsImage"></img></a>' +
+		'<h3>' + result['title'] + '</h3>' + '<h4>' + result['date'] + '</h4>' +
+		'<p>' + result['summary'] + '</p>');
 }
 
 /*
@@ -35,4 +46,21 @@ function addProjectDetails(e) {
  */
 function randomizeColors(e) {
 	console.log("User clicked on color button");
+	$.get("/palette", fillColors);
 }
+
+function fillColors(result) {
+	var colors = result['colors']['hex'];
+	$('body').css('background-color', colors[0]);
+	$('.thumbnail').css('background-color', colors[1]);
+	$('h1, h2, h3, h4, h5, h5').css('color', colors[2]);
+	$('p').css('color', colors[3]);
+	$('.project img').css('opacity', .75);
+}
+
+
+
+
+
+
+
